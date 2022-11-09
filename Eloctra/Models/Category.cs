6eb5ebc63526423a4bfc7aec6e44dcb63df3fb79
Dhruvi Sherathiya <83078196+DhruviSherathiya@users.noreply.github.com>
2022-11-09 10:26:14 +0530

    Category Fixed

--- Eloctra/Models/Category.cs
+++ Eloctra/Models/Category.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Eloctra.Data
+namespace Eloctra.Models
 {
     public enum Category
     {
@@ -13,6 +14,6 @@ namespace Eloctra.Data
         Accessories,
         Mobiles,
         Wearables
-    }
 
+    }
 }
